53afbf6742cd2aac1b23d8d0997b7be766465af3
sergioromeroo <manusergio96@gmail.com>
2022-04-25 08:44:05 -0300

    andando

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,10 @@
-require('dotenv').config()
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
+    username: "root",
+    password: "42871731",
+    database: "crafsty3.0",
+    host: "127.0.0.1",
+    dialect: "mysql"
   },
   test: {
     username: "root",
